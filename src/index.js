cd375b3219a98671e6339d8fae5959a11ff57f5c
Vitor Manoel Santos Moura <v.program.py@gmail.com>
2023-08-15 20:05:27 -0300

    commit 2

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const http = require('http'); // importar http
+
+// criando servidor
+const server = http.createServer((request, response) => { 
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('<h1>Hello World!</h1>');
+});
+
+server.listen(3000, ()=> console.log('💕 server started at http://localhost:3000'));
